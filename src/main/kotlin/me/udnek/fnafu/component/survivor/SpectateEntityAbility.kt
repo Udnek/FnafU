6e5e94183a40c6c1a45710a3fefeffafdf7f5ccc
PUBKABOZKAX <g_churakov@list.ru>
2025-04-17 12:37:42 +0300

    tablet (full models, definitions and textures)

--- src/main/kotlin/me/udnek/fnafu/component/survivor/SpectateEntityAbility.kt
+++ src/main/kotlin/me/udnek/fnafu/component/survivor/SpectateEntityAbility.kt
@@ -4,18 +4,20 @@ package me.udnek.fnafu.component.survivor
 import com.comphenix.protocol.PacketType
 import com.comphenix.protocol.ProtocolLibrary
 import me.udnek.fnafu.component.Abilities
+import me.udnek.fnafu.player.FnafUPlayer
 import me.udnek.itemscoreu.customcomponent.CustomComponent
 import me.udnek.itemscoreu.customcomponent.CustomComponentType
 import me.udnek.itemscoreu.customminigame.ability.MGUAbilityHolderComponent
 import me.udnek.itemscoreu.customminigame.ability.MGUAbilityInstance
-import me.udnek.itemscoreu.customminigame.player.MGUPlayer
 import org.bukkit.entity.Entity
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack
 
 open class SpectateEntityAbility : MGUAbilityInstance {
 
     companion object {
         val DEFAULT = object : SpectateEntityAbility() {
-             override fun spectate(player: MGUPlayer, target: Entity){
+             override fun spectate(player: FnafUPlayer, target: Entity){
                  throw RuntimeException("Component is default!")
              }
         }
@@ -24,7 +26,7 @@ open class SpectateEntityAbility : MGUAbilityInstance {
     var spectatingEntity: Entity? = null
         protected set
 
-    open fun spectate(player: MGUPlayer, target: Entity){
+    open fun spectate(player: FnafUPlayer, target: Entity){
         val protocolManager = ProtocolLibrary.getProtocolManager()
         val packet = protocolManager.createPacket(PacketType.Play.Server.CAMERA)
         packet.integers.write(0, target.entityId)
@@ -32,8 +34,12 @@ open class SpectateEntityAbility : MGUAbilityInstance {
         spectatingEntity = if (player.player == target) null else target
     }
 
-    fun spectateSelf(player: MGUPlayer){
+    fun spectateSelf(player: FnafUPlayer){
         spectate(player, player.player)
+    }
+
+    fun getItem(player: FnafUPlayer): ItemStack {
+        return player.player.inventory.getItem(EquipmentSlot.HAND)
     }
 
     override fun getType(): CustomComponentType<out MGUAbilityHolderComponent, out CustomComponent<MGUAbilityHolderComponent>> {
