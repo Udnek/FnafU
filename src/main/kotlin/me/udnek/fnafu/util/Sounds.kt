6b686e325e5f51a3f01d8cebfe1fa022cf4c0b75
PUBKABOZKAX <g_churakov@list.ru>
2025-06-01 18:40:53 +0300

    JUMP SCARE

--- src/main/kotlin/me/udnek/fnafu/util/Sounds.kt
+++ src/main/kotlin/me/udnek/fnafu/util/Sounds.kt
@@ -19,7 +19,7 @@ object Sounds {
     val SPRINGTRAP_SPAWN : CustomSound = register(ConstructableCustomSound(getName("springtrap_spawn"), SoundCategory.HOSTILE))
     val SPRINGTRAP_RAGE_START : CustomSound = register(ConstructableCustomSound(getName("springtrap_rage_start"), SoundCategory.HOSTILE))
     val SPRINGTRAP_RAGE_STOP : CustomSound = register(ConstructableCustomSound(getName("springtrap_rage_stop"), SoundCategory.HOSTILE))
-    val SPRINGTRAP_SCREAMER : CustomSound = register(ConstructableCustomSound(getName("springtrap_screamer"), SoundCategory.HOSTILE))
+    val SPRINGTRAP_JUMP_SCARE : CustomSound = register(ConstructableCustomSound(getName("springtrap_jump_scare"), SoundCategory.HOSTILE))
 
     val PLUSHTRAP_RUN : CustomSound = register(ConstructableCustomSound(getName("plushtrap_run"), SoundCategory.HOSTILE))
     val PLUSHTRAP_NEAR : CustomSound = register(ConstructableCustomSound(getName("plushtrap_near"), SoundCategory.HOSTILE))
