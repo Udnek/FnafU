e9b178a8f5b65259dfec413112f1fd4ac67b3283
PUBKABOZKAX <g_churakov@list.ru>
2025-06-26 21:09:57 +0300

    springtrap skin, springtrap mask

--- src/main/kotlin/me/udnek/fnafu/item/Items.kt
+++ src/main/kotlin/me/udnek/fnafu/item/Items.kt
@@ -15,28 +15,28 @@ import me.udnek.fnafu.item.survivor.doorman.DoormanTabletButton
 
 
 object Items {
-    @JvmField val CAMERA_BUTTON: CustomItem = register(CameraButton())
-    @JvmField val CAMERA_OVERLAY: CustomItem = register(EmptyItem("camera_overlay"))
+    val CAMERA_BUTTON: CustomItem = register(CameraButton())
+    val CAMERA_OVERLAY: CustomItem = register(EmptyItem("camera_overlay"))
 
-    @JvmField val FULL_CAMERA_TABLET: CustomItem = register(FullCameraTablet())
-    @JvmField val CUT_CAMERA_TABLET: CustomItem = register(CutCameraTablet())
+    val FULL_CAMERA_TABLET: CustomItem = register(FullCameraTablet())
+    val CUT_CAMERA_TABLET: CustomItem = register(CutCameraTablet())
 
-    @JvmField val SYSTEM_TABLET: CustomItem = register(SystemTablet())
+    val SYSTEM_TABLET: CustomItem = register(SystemTablet())
 
-    @JvmField val DOOR_TABLET: CustomItem = register(DoormanTablet())
-    @JvmField val DOOR_BUTTON: CustomItem = register(DoormanTabletButton())
+    val DOOR_TABLET: CustomItem = register(DoormanTablet())
+    val DOOR_BUTTON: CustomItem = register(DoormanTabletButton())
 
-    @JvmField val UP_BUTTON: CustomItem = register(EmptyItem("up_button").hiddenModel())
-    @JvmField val DOWN_BUTTON: CustomItem = register(EmptyItem("down_button").hiddenModel())
-    @JvmField val ENTER_BUTTON: CustomItem = register(EmptyItem("enter_button").hiddenModel())
-    @JvmField val EXIT_BUTTON: CustomItem = register(EmptyItem("exit_button").hiddenModel())
-    @JvmField val CURSOR_ICON: CustomItem = register(EmptyItem("cursor"))
-    @JvmField val ERROR_ICON: CustomItem = register(EmptyItem("error"))
-    @JvmField val REBOOT_ICON: CustomItem = register(EmptyItem("reboot"))
+    val UP_BUTTON: CustomItem = register(EmptyItem("up_button").hiddenModel())
+    val DOWN_BUTTON: CustomItem = register(EmptyItem("down_button").hiddenModel())
+    val ENTER_BUTTON: CustomItem = register(EmptyItem("enter_button").hiddenModel())
+    val EXIT_BUTTON: CustomItem = register(EmptyItem("exit_button").hiddenModel())
+    val CURSOR_ICON: CustomItem = register(EmptyItem("cursor"))
+    val ERROR_ICON: CustomItem = register(EmptyItem("error"))
+    val REBOOT_ICON: CustomItem = register(EmptyItem("reboot"))
 
-    @JvmField val SPRINGTRAP_PLUSHTRAP_ABILITY: CustomItem = register(SpringtrapPlushtrapItem())
-    @JvmField val SPRINGTRAP_BREAK_CAMERAS: CustomItem = register(SpringtrapBreakCamerasItem())
-    @JvmField val SPRINGTRAP_MASK: CustomItem = register(SpringtrapMask())
+    val SPRINGTRAP_PLUSHTRAP_ABILITY: CustomItem = register(SpringtrapPlushtrapItem())
+    val SPRINGTRAP_BREAK_CAMERAS: CustomItem = register(SpringtrapBreakCamerasItem())
+    val SPRINGTRAP_MASK: CustomItem = register(SpringtrapMask())
 
     private fun register(customItem: CustomItem): CustomItem {
         return CustomRegistries.ITEM.register(FnafU.instance, customItem)
