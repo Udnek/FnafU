538dc14a0c11db8e711eba9d68d7b9886bf19eae
PUBKABOZKAX <g_churakov@list.ru>
2025-09-19 17:35:38 +0300

    freddy trap show radius

--- src/main/kotlin/me/udnek/fnafu/entity/remnanttrap/RemnantTrap.kt
+++ src/main/kotlin/me/udnek/fnafu/entity/remnanttrap/RemnantTrap.kt
@@ -5,7 +5,10 @@ import me.udnek.coreu.custom.entitylike.entity.CustomTickingEntityType
 import me.udnek.coreu.rpgu.component.RPGUComponents
 import me.udnek.fnafu.component.animatronic.freddy.FreddyTrapData
 import me.udnek.fnafu.entity.EntityTypes
+import me.udnek.fnafu.misc.Utils
 import me.udnek.fnafu.player.FnafUPlayer
+import org.bukkit.Color
+import org.bukkit.Particle
 import org.bukkit.entity.Item
 
 class RemnantTrap : ConstructableCustomEntity<Item>() {
@@ -20,13 +23,16 @@ class RemnantTrap : ConstructableCustomEntity<Item>() {
     override fun delayedTick() {
         if (!this::data.isInitialized) return
 
+        val radius = data.ability!!.components.getOrDefault(RPGUComponents.ABILITY_AREA_OF_EFFECT).get(data.player!!.player)
         data.player!!.game.findNearbyPlayers(
             entity.location,
-            data.ability!!.components.getOrDefault(RPGUComponents.ABILITY_AREA_OF_EFFECT).get(data.player!!.player),
+            radius,
             FnafUPlayer.Type.SURVIVOR
         ).forEach {
                 it.showAuraTo(data.player!!.game.playerContainer.animatronics, SCAN_DURATION)
         }
+        Utils.spawnDustCircle(Particle.DUST.builder().color(Color.AQUA).location(entity.location).receivers(data.player!!.player), radius, 0.1,
+            {return@spawnDustCircle !it.block.isSolid})
     }
 
     override fun getType(): CustomTickingEntityType<*> = EntityTypes.REMNANT_TRAP
